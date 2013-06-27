ca4e6a28dcd20020003c4ea90ae5ae54f4e4e40e
Tobias Bohwalli <hi@futhr.io>
2013-06-27 22:31:08 +0200

    Base order controller decorator.

---
+++ app/controllers/spree/admin/order_controller_decorator.rb
@@ -0,0 +1,4 @@
+module Spree
+  Admin::OrdersController.class_eval do
+  end
+end
---
+++ spec/controllers/spree/admin/orders_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Spree::Admin::OrdersController do
+  stub_authentication!
+
+  context "" do
+  end
+end
