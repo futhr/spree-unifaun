5e766a65004196699ac1e0eba44b8cd561541ad4
abhishek <bigbeliever@gmail.com>
2013-07-04 06:46:52 +0530

    1. Unifaun::ShipmentController#to_xml nearly complete. 2. Config changes and additional matcher in spec_helper.rb -- sponsored by @futher --

--- app/models/spree/unifaun/shipment_controller.rb
+++ app/models/spree/unifaun/shipment_controller.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 
 class Spree::Unifaun::ShipmentController
   include HTTParty
-  base_uri 'api.unifaum.com'
+  base_uri 'api.unifaun.com'
   format :xml
 
   attr_reader :shipment, :order, :address
@@ -27,19 +27,27 @@ class Spree::Unifaun::ShipmentController
 		builder = Nokogiri::XML::Builder.new(encoding: "ISO-8859-1") do |xml|
 			xml.data{
 				xml.receiver(rcvid: @address.id){
-					xml.val(address_name,         n: :name) 
-					xml.val(@address.address1,    n: :address1) 
-					xml.val(@address.address2,    n: :address2) 
-					xml.val(@address.zipcode,     n: :zipcode)
-					xml.val(@address.city,        n: :city)
-					xml.val(country_iso,          n: :country)
-					xml.val(order_email,          n: :email)
-					xml.val(@address.phone,       n: :phone)
-					xml.val("sms",                n: :sms)
-					xml.val("contact",            n: :contact)
+					xml.val address_name,         n: :name
+					xml.val @address.address1,    n: :address1 
+					xml.val @address.address2,    n: :address2
+					xml.val @address.zipcode,     n: :zipcode
+					xml.val @address.city,        n: :city
+					xml.val country_iso,          n: :country
+					xml.val order_email,          n: :email
+					xml.val @address.phone,       n: :phone
 				}
 				xml.shipment(orderno: @order.number){
-					xml.from ""
+					xml.val shipment_from,        n: :from
+					xml.val shipment_to,          n: :to
+					xml.val shipment_shipdate,    n: :shipdate
+					xml.service "",               srvid: service_id 
+					xml.container(type: container_type){
+						xml.val container_copies,   n: :copies
+						xml.val container_weight,   n: :weight 
+						#xml.val container_length,   n: :length
+						#xml.val container_width,    n: :width 
+						#xml.val container_height,   n: :height
+					}
 				}
 			}
 		end 
@@ -58,6 +66,45 @@ class Spree::Unifaun::ShipmentController
 		@address.country.iso_name
 	end
 
+	def shipment_from 
+		Spree::Unifaun::Config.preferred_quick_id_for_sender
+	end
+
+	def shipment_to # ShipAddress#id
+		@address.id
+	end
+
+	def shipment_shipdate  # Defines shipment date. Printed on shipping documents. The default value is the current date. 
+		@shipment.created_at.to_date
+	end
+
+	def service_id  
+		Spree::Unifaun::Config.preferred_service
+	end
+
+	def container_type ## 
+		"parcel"
+	end
+
+	def container_copies ## 
+		1
+	end
+
+	def container_weight ## Variant#weight
+		@shipment.to_package.weight
+	end
+
+	#def container_length ## Variant#length
+	#	return nil if @order.line_items.any?{|item| item.weight.nil? }
+	#end
+	#
+	#def container_width  ## Variant#width
+	#	10
+	#end
+	#
+	#def container_height ## Variant#height
+	#	15
+	#end
 
 	
 end
--- spec/models/spree/unifaun/shipment_controller_spec.rb
+++ spec/models/spree/unifaun/shipment_controller_spec.rb
@@ -3,132 +3,138 @@ describe Spree::Unifaun::ShipmentController do
 	
 	let(:unifaun_shipment_controller) { Spree::Unifaun::ShipmentController.new(create(:shipment)) }
 
-  context "#new" do 
-  	it "prepares a unifaum-shipment from given Spree::Shipment object" do
-  	 pending "TODO"
-  	end 
-	end
-
   context "#post" do       
-  	it "notifies Unifaum Admin of a new Spree::Shipment" do
+  	it "notifies Unifaun of a new Spree::Shipment" do
   		pending "TODO"
   	end
 	end
 
   context "#get_status" do 
-  	it "gets the status of a Spree::Shipment from Unifaum" do
+  	it "gets the status of a Spree::Shipment from Unifaun" do
 		 pending "TODO"
 		end
 	end
   
-
   context "#to_xml" do 
   	it "has root element with the encoding ISO-8859-1" do
-      xml = unifaun_shipment_controller.to_xml
-      xml.should match(/encoding=\"ISO-8859-1\"/)
+      unifaun_shipment_controller.to_xml.should match(/encoding=\"ISO-8859-1\"/)
   	end
 
-    describe ":receiver element:" do
+    describe "<receiver> element: " do
     	it "should be present" do
-    	  xml = unifaun_shipment_controller.to_xml
-    	  xml.should match(/<receiver/)
+        unifaun_shipment_controller.to_xml.should have_xml("/data/receiver")
     	end
 
       it "has the address's id as rcvid" do
-        xml = unifaun_shipment_controller.to_xml
         address_id = unifaun_shipment_controller.address.id
-        xml.should match(/<receiver rcvid=\"#{address_id}\"/) 
+        unifaun_shipment_controller.to_xml.should have_xml("/data/receiver[@rcvid=#{address_id}]")
       end
 
       it "has name as Unifaun AB" do
         unifaun_shipment_controller.address.firstname = "Unifaun"
         unifaun_shipment_controller.address.lastname = "AB"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="name">Unifaun AB<\/val>/)
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="name"]', "Unifaun AB")
       end
 
       it "has address1 as 5, 7th avenue" do
         unifaun_shipment_controller.address.address1 = "5, 7th avenue"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="address1">5, 7th avenue<\/val>/)        
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="address1"]', "5, 7th avenue")        
       end
 
       it "has address2 as Kaserntorget" do
         unifaun_shipment_controller.address.address2 = "Kaserntorget"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="address2">Kaserntorget<\/val>/)           
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="address2"]', "Kaserntorget")
       end
 
       it "has zipcode as 97469" do
         unifaun_shipment_controller.address.zipcode = "97469"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="zipcode">97469<\/val>/)        
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="zipcode"]', "97469")
       end
 
       it "has city as BERLIN" do
-        unifaun_shipment_controller.address.city = "BERLIN"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="city">BERLIN<\/val>/)        
+        unifaun_shipment_controller.address.city = "BERLIN"  
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="city"]', "BERLIN")
       end
 
       it "has country as DE" do
         unifaun_shipment_controller.address.country.iso_name = "DE"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="country">DE<\/val>/)        
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="country"]', "DE")
       end
 
       it "has email as support@unifaun.se" do
         unifaun_shipment_controller.order.user.email = "support@unifaun.se"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="email">support@unifaun.se<\/val>/)        
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="email"]', "support@unifaun.se")
       end
 
       it "has phone as 031-725 35 00" do
         unifaun_shipment_controller.address.phone = "031-725 35 00"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="phone">031-725 35 00<\/val>/)        
-      end
-
-      it "has sms as 0700-000000" do
-        pending "SPREE ADDRESS HAS NO SEPARATE MOBILE NO."
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="sms">0700-000000<\/val>/)        
-      end
-
-      it "has contact as Support" do
-        pending "SPREE ADDRESS HAS NO SEPARATE CONTACT PERSON"
-        xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<val n="contact">Support<\/val>/)        
+        unifaun_shipment_controller.to_xml.should have_xml('/data/receiver/val[@n="phone"]', "031-725 35 00")
       end
 
     end
 
-    describe ":shipment element:" do
+    describe "<shipment> element: " do
     	it "should be present" do
-    	  xml = unifaun_shipment_controller.to_xml
-        xml.should match(/<shipment/)
+        unifaun_shipment_controller.to_xml.should have_xml '/data/shipment'
     	end
 
       it "has the order's number as orderno" do
-        xml = unifaun_shipment_controller.to_xml
         order_no = unifaun_shipment_controller.order.number
-        xml.should match(/<shipment orderno=\"#{order_no}\"/)
+        unifaun_shipment_controller.to_xml.should have_xml "/data/shipment[@orderno=\"#{order_no}\"]"
       end
+
+      it "has *from* as 123 - the sender's quick id" do
+        Spree::Unifaun::Config.preferred_quick_id_for_sender=123
+        unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/val[@n="from"]', 
+          "123"
+      end
+
+      it "has *to* as the ship-address id" do
+        unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/val[@n="to"]', 
+          unifaun_shipment_controller.address.id.to_s
+      end
+
+      it "has *shipdate* as 2013-07-01 when the shipments is created-at 2013-07-01" do
+        unifaun_shipment_controller.shipment.created_at = DateTime.parse("2013-07-01")
+        unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/val[@n="shipdate"]', 
+          "2013-07-01"
+      end
+
+      describe "<service> element: " do
+
+        it "should be present" do
+          unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/service'
+        end
+
+        it "has the 'srvid' as APL when chosen service is APL" do
+          Spree::Unifaun::Config.preferred_service = "APL"
+          unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/service[@srvid="APL"]'
+        end
+
+      end
+
+      describe "<container> element: " do
+        it "should be present" do
+          unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/container'
+        end
+
+        it "has the 'type' as 'parcel'" do
+          unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/container[@type="parcel"]'
+        end
+
+        it "has copies as 1 when there is one parcel in the package" do
+          unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/container/val[@n="copies"]', "1"
+        end
+
+        it "has weight as 4 when there are two items in the order of weight 2kg each" do
+          pending "WIP"
+          unifaun_shipment_controller.to_xml.should have_xml '/data/shipment/container/val[@n="weight"]', "4"
+        end
+
+      end
+
     end
   end	
-
-
-  context "#save" do
-    it "saves the unifaum-shipment to the database" do
-    	pending "TODO"
-    end
-  end
-
-  context "#unifaum_id" do 
-  	it "gives the identifier of the shipment on Unifaum" do
-  		pending "TODO"
-  	end
-  end
 
 
 end
