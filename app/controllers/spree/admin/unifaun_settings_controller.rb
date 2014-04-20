4d0df91fb093d375888b80eff879571b9cd2006a
Tobias Bohwalli <hi@futhr.io>
2014-04-20 07:14:34 +0200

    Refactor Settings.

--- app/controllers/spree/admin/unifaun_settings_controller.rb
+++ app/controllers/spree/admin/unifaun_settings_controller.rb
@@ -2,14 +2,14 @@ module Spree
   module Admin
     class UnifaunSettingsController < ResourceController
 
-      # save settings
       def update
         settings = Spree::UnifaunSetting.new
-        params[:preferences].each do |k,v|
-          settings[k] = v
+        params.each do |name, value|
+          next unless settings.has_preference? name
+          settings[name] = value
         end
         flash[:success] = Spree.t(:successfully_updated, resource: Spree.t(:settings, scope: :unifaun))
-        render :edit
+        redirect_to edit_admin_unifaun_settings_path
       end
     end
   end
--- spec/features/admin/unifaun_settings_spec.rb
+++ spec/features/admin/unifaun_settings_spec.rb
@@ -11,25 +11,27 @@ feature 'Admin Settings for Unifaun', :js do
   end
 
   scenario 'update' do
-    expect(page).to have_text 'Track and Trace Language'.upcase
+    expect(page).to have_text_like 'Track and Trace Language'
 
-    select 'Pacsoft Online', from: 'preferences_service'
-    fill_in 'preferences_user_id', with: '12'
-    fill_in 'preferences_password', with: '123'
-    fill_in 'preferences_quick_id_for_sender', with: '1234'
-    fill_in 'preferences_weight_multiplyer', with: '1.5'
-    select 'Swedish', from: 'preferences_track_and_trace_language'
+    fill_in 'user_id', with: '12'
+    fill_in 'password', with: '123'
+    fill_in 'quick_id_for_sender', with: '1234'
+    fill_in 'weight_multiplyer', with: '1.5'
+
+    select2 'Pacsoft Online', from: 'Service'
+    select2 'Swedish', from: 'Track and Trace Language'
 
     click_button 'Update'
 
     expect(page).to have_text 'successfully updated!'
 
     setting = Spree::UnifaunSetting.new
-    expect(setting.preferred_service).to eq 'Pacsoft Online'
+
     expect(setting.preferred_user_id).to eq 12
     expect(setting.preferred_password).to eq '123'
     expect(setting.preferred_quick_id_for_sender).to eq 1234
     expect(setting.preferred_weight_multiplyer).to eq '1.5'
+    expect(setting.preferred_service).to eq 'Pacsoft Online'
     expect(setting.preferred_track_and_trace_language).to eq 'Swedish'
   end
 end
