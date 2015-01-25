8b8bcc32a3b0211b39ab74384e834c995cf06037
Tobias Bohwalli <hi@futhr.io>
2015-01-25 19:24:45 +0100

    Remove Deface option "disabled".
    Its unnecessary and just may raise deprecation warnings.

--- app/overrides/add_unifaun_to_admin_configuration_sidebar.rb
+++ app/overrides/add_unifaun_to_admin_configuration_sidebar.rb
@@ -2,6 +2,5 @@ Deface::Override.new(
   virtual_path:  'spree/admin/shared/sub_menu/_configuration',
   name:          'unifaun_admin_configurations_menu',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  text:          '<%= configurations_sidebar_menu_item Spree.t(:settings, scope: :unifaun), edit_admin_unifaun_settings_path %>',
-  disabled:      false
+  text:          '<%= configurations_sidebar_menu_item Spree.t(:settings, scope: :unifaun), edit_admin_unifaun_settings_path %>'
 )
